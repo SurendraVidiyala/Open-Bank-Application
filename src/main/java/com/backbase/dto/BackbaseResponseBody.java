dfd4d57705a78b11a3e0570d20dad71b0e79c94c
Arka Bhattacharya <bhattacharyaarka11@gmail.com>
2018-06-24 18:13:30 +0530

    Response body added

---
+++ src/main/java/com/backbase/dto/BackbaseResponseBody.java
@@ -0,0 +1,17 @@
+package com.backbase.dto;
+
+public class BackbaseResponseBody {
+	
+	private String id;
+	private String accountId;
+	private String counterpartyAccount;
+	private String counterpartyName;
+	private String counterPartyLogoPath;
+	private String instructedAmount;
+	private String instructedCurrency;
+	private String transactionAmount;
+	private String transactionCurrency;
+	private String transactionType;
+	private String description;
+
+}
