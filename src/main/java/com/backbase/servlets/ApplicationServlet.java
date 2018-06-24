73e682e8e8fb3c4f83d3cf93e8f4d74784827ac6
Arka Bhattacharya <bhattacharyaarka11@gmail.com>
2018-06-24 12:26:06 +0530

    Removed earlier version of Application.java, servlet. need to look at the DispatcherServlet

--- src/main/java/com/backbase/servlets/ApplicationServlet.java
+++
@@ -1,8 +0,0 @@
-package com.backbase.servlets;
-
-import javax.servlet.http.HttpServlet;
-
-//@WebServlet("/transactions")
-public class ApplicationServlet extends HttpServlet{
-	private static final long serialVersionUID = 1L;
-}
